1b0de0ca76809a0626d243baedab14d28d7f7ee9
Adrián Pelayo <niniohhns@gmail.com>
2022-05-02 13:42:00 +0200

    React Masterclass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  // First Lesson
+  const [name, setName] = useState("Adri");
+
+  // Second Lesson
+  const [count, setCount] = useState(0);
+
+  // Third Lesson
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // First Lesson
+  const handleHejjButton = () => {
+    setName("Nanny");
+  }
+
+  // Second Lesson
+  const handleCountButtons = (e) => {
+    const event = e.target;
+    switch(event.id) {
+      case "substract":
+        setCount(count-1);
+        break;
+      case "add":
+        setCount(count+1);
+        break;
+    }
+  }
+
+  // Third Lesson
+  const handleChangeLoginInputs = (e) => {
+    const event = e.target;
+    console.log(event);
+    switch(event.id) {
+      case "username":
+        setEmail(event.value);
+        break;
+      case "password":
+        setPassword(event.value);
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="firstLesson">
+        <h1>Hola {name}</h1>
+        <button onClick={handleHejjButton}>Hejj</button>
+      </div>
+      <div className="secondLesson">
+        <button onClick={handleCountButtons} id="substract">-1</button>
+        <span>{count}</span>
+        <button onClick={handleCountButtons} id="add">+1</button>
+      </div>
+      <div className="thirdLesson">
+        <h2>Iniciar Sesión</h2>
+        <label for="username">Usuario: <input type="email" id="username" name="username" value={email} onChange={handleChangeLoginInputs}/></label>
+        <label for="password">Contraseña: <input type="password" id="password" name="password" value={password} onChange={handleChangeLoginInputs}/></label>
+      </div>
     </div>
   );
 }
